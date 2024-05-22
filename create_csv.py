50a7d35a45be71ee8b7e1f8318b3f0f61777dc2f
JJ Asghar <awesome@ibm.com>
2024-05-22 17:02:16 -0500

    Fixing the number of columns.
    too many columns.
    
    Signed-off-by: JJ Asghar <awesome@ibm.com>

--- create_csv.py
+++ create_csv.py
@@ -13,7 +13,7 @@ def main(repo):
     if os.path.exists(csv_file):
         with open(csv_file, 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow([f'{today_date}',f'{total_open_pulls(repo)}',f'{total_triage_requested_changes_labeled(repo)}',f'{total_triage_needed_prs(repo)}',f'{total_triage_requested_changes_and_triage_needed_labeled(repo)}',f'{total_precheck_generate_ready(repo)}',f'{total_triage_rejected(repo)}',f'{total_stale_prs(repo)}',f'{total_stale_prs(repo)}',f'{total_stale_prs(repo)}',f'{total_stale_prs(repo)}'])
+            writer.writerow([f'{today_date}',f'{total_open_pulls(repo)}',f'{total_triage_requested_changes_labeled(repo)}',f'{total_triage_needed_prs(repo)}',f'{total_triage_requested_changes_and_triage_needed_labeled(repo)}',f'{total_precheck_generate_ready(repo)}',f'{total_triage_rejected(repo)}',f'{total_stale_prs(repo)}'])
     else:
         with open(csv_file, 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
